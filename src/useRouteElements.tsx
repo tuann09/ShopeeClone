c6da2f688adfbb25232319ab7877d5fd4b2d0bbd
Tuans09 <dt2003911@gmail.com>
2025-07-30 10:30:47 +0700

    cau hinh react router

---
+++ src/useRouteElements.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import ProductList from './pages/ProductList'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import RegisterLayout from './layout/RegisterLayout'
+
+export default function useRouteElements() {
+  const RouteElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return RouteElements
+}
